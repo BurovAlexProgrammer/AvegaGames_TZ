63b116f2f127fe4429d710b637c0c2cef61acb74
Aleksey <burovalexprogrammer@gmail.com>
2022-04-23 07:43:23 +0200

    Added AudioEvent

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -54,3 +54,18 @@ public static class Extensions
         return null;
     }
 }
+
+[Serializable]
+public struct RangedFloat {
+    public float minValue;
+    public float maxValue;
+}
+
+public class MinMaxRangeAttribute : Attribute {
+    public MinMaxRangeAttribute(float min, float max) {
+        Min = min;
+        Max = max;
+    }
+    public float Min { get; private set; }
+    public float Max { get; private set; }
+}
---
+++ Assets/Scripts/SimpleAudioEvent.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/Audio Events/Simple")]
+public class SimpleAudioEvent : AudioEvent
+{
+    public AudioClip[] audioClips;
+    
+    [MinMaxRange(0,1)]
+    public RangedFloat volume;
+
+    [MinMaxRangeAttribute(0,2)]
+    public RangedFloat pitch;
+
+    public override void Play(AudioSource audioSource) {
+        if (audioClips.Length == 0) return;
+        
+		audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+		audioSource.volume = Random.Range(volume.minValue, volume.maxValue);
+		audioSource.pitch = Random.Range(pitch.minValue, pitch.maxValue);
+		audioSource.Play();
+    }
+}
